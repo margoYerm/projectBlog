8eeac5519718ef6b45a03ca0440adc832d6f6428
Margo Yermachenko <margoermachenko@gmail.com>
2023-12-13 02:31:27 +0100

    Added feature edit article

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -37,5 +37,10 @@ export const appRoutes: Route[] = [
         path: 'articles/:slug',
         loadChildren: () => import('src/app/article/article.routes')
         .then((m) => m.routes)
-    },    
+    },  
+    {
+        path: 'articles/:slug/edit',
+        loadChildren: () => import('src/app/editArticle/editArticle.routes')
+        .then((m) => m.routes)
+    },  
 ]
