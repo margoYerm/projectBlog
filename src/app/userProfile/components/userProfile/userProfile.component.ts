078bb3be1bad5290cfeabff3d0260a53a0beb9b8
Margo Yermachenko <margoermachenko@gmail.com>
2023-12-15 02:47:44 +0100

    Added userPrifile feature

---
+++ src/app/userProfile/components/userProfile/userProfile.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { Store, select } from "@ngrx/store";
+import { userProfileActions } from "../../store/actions";
+import { combineLatest, filter, map } from "rxjs";
+import { selectError, selectIsLoading, selectUserProfileData } from "../../store/reducers";
+import { selectCurrentUser } from "src/app/auth/store/reducers";
+import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
+import { UserProfileInterface } from "../../types/userProfile.interface";
+import { CommonModule } from "@angular/common";
+import { FeedComponent } from "../../../shared/components/feed/feed.component";
+
+@Component({
+    selector: 'apm-user-profile',
+    templateUrl: './userProfile.component.html',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterLink,
+        RouterLinkActive,
+        FeedComponent,
+    ]
+})
+
+export class UserProfileComponent implements OnInit {
+  slug: string = ''
+
+  isCurrentUserProfile$ = combineLatest({
+    currentUser: this.store.pipe(
+      select(selectCurrentUser),
+      filter((currentUser): currentUser is CurrentUserInterface => 
+        Boolean(currentUser)
+      )      
+    ),
+
+    userProfile: this.store.pipe(
+      select(selectUserProfileData),
+      filter((userProfile): userProfile is UserProfileInterface => 
+        Boolean(userProfile)
+      )      
+    ),
+  }).pipe(
+    map(({currentUser, userProfile}) => {
+      return currentUser.username === userProfile.username
+    })
+  )
+
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    userProfile: this.store.select(selectUserProfileData),
+    isCurrentUserProfile: this.isCurrentUserProfile$ 
+  })  
+
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.slug = params['slug']
+      this.fetchUserProfile()
+    })
+  }
+
+  fetchUserProfile(): void {
+    this.store.dispatch(userProfileActions.getUserProfile({slug: this.slug}))
+  }
+
+  getApiUrl() {
+    const isFavorites = this.router.url.includes('favorites')
+    return isFavorites ? `/articles?favorited=${this.slug}` 
+    : `/articles?author=${this.slug}`      
+  }
+}
